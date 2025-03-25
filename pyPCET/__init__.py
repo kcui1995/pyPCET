a89bf649d34f1efffd7ee2b1861d88f986b949a3
kcui1995 <kcui1995@gmail.com>
2025-03-25 15:02:09 -0400

    Update __init__.py

--- pyPCET/__init__.py
+++ pyPCET/__init__.py
@@ -3,11 +3,13 @@ from .units import *
 from .pyPCET import pyPCET
 from .FGH_1D import FGH_1D
 from .nonadiabaticity import kappa_coupling
+from .electrochem import *
 
 __all__ = ['Morse', 'Morse_inverted', 'Gaussian', 'poly6', 'poly8',
            'gen_Morse', 'gen_Morse_inverted', 'gen_double_well', 'fit_poly6', 'fit_poly8', 'bspline', 
            'find_roots', 'isnumber', 'isarray', 'copy_func', 
            'FGH_1D', 'pyPCET', 'kappa_coupling', 
+           'EDL_model', 'Fermi', 
            'kB', 'h', 'hbar', 'c', 'massH', 'massD',
            'Ha2eV', 'Ha2kcal', 'kcal2Ha', 'eV2Ha', 'eV2kcal', 'kcal2eV',
            'A2Bohr', 'A2nm', 'A2cm', 'Bohr2A', 'cm2A', 'nm2A', 'wn2eV', 'eV2wn', 'Da2me', 'me2Da', 'au2s', 
